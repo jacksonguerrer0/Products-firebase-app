fd1dbfcbe40cab482f0ad2fd2445caa52fa6ac51
Jackson Guerrero <jacksonguerrerovega2@gmail.com>
2021-07-24 16:30:22 -0500

    Mejora

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Redirect, Switch } from 'react-router-dom'
 import Products from '../container/Products'
 import firebase from 'firebase'
 import { useDispatch } from 'react-redux'
@@ -7,6 +7,7 @@ import { login } from '../redux/loguinDucks'
 import PrivateRoute from './PrivateRoute'
 import Register from '../components/Register'
 import Login from '../components/Login'
+import PublicRoute from './PublicRoute'
 
 
 const Routes = () => {
@@ -36,19 +37,21 @@ const Routes = () => {
     return (
         <Router>
             <Switch>
-                <Route 
+                <PublicRoute 
+                isAuthenticated={isLoggedIn}
                     exact
                     path="/auth/login"
                     component={ Login }
                 />
 
-                <Route 
+                <PublicRoute 
+                isAuthenticated={isLoggedIn}
                     exact
                     path="/auth/register"
                     component={ Register }
                 />
                 <PrivateRoute isAuthenticated={isLoggedIn} exact path='/' component={Products} />
-                {/* <Redirect to='/auth/login'/>  */}
+                <Redirect to='/auth/login'/> 
             </Switch>
         </Router>
     )
